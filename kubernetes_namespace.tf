b7a2c4f9c18626726b5529c6c0bd19c71f91fabb
Jachin <82848023+wardaddy9@users.noreply.github.com>
2023-06-15 14:29:11 -0500

    Update kubernetes_namespace.tf

--- kubernetes_namespace.tf
+++ kubernetes_namespace.tf
@@ -3,11 +3,11 @@ resource "kubernetes_namespace_v1" "ns" {
 
   metadata {
     name = var.kube_namespace
-
+#Annotations are key-value pairs used to attach arbitrary metadata to Kubernetes objects
     annotations = {
       name = "sample-annotation"
     }
-
+#Labels are key-value pairs attached to Kubernetes objects to identify and group related resources
     labels = {
       tier = "frontend"
     }
