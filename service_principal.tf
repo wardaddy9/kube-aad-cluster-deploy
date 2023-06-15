46ef61a667782fcb003d74ffbb002be7f62d7e59
Jachin <82848023+wardaddy9@users.noreply.github.com>
2023-06-15 14:32:30 -0500

    Update service_principal.tf

--- service_principal.tf
+++ service_principal.tf
@@ -1,3 +1,4 @@
+#Creating service principal that we will use to deploy to the cluster
 resource "azuread_application" "app" {
   display_name = var.spn_name
 }
@@ -6,16 +7,16 @@ resource "azuread_application" "app" {
 resource "azuread_service_principal" "spn" {
   application_id = azuread_application.app.application_id
 }
-
+#The above principal and password we be used in providers.tf file login to AKS cluster
 resource "azuread_service_principal_password" "spn_password" {
   service_principal_id = azuread_service_principal.spn.id
 }
-
+#Creating the AAD group
 resource "azuread_group" "aks_admins" {
   display_name     = var.aad_group_aks_admins
   security_enabled = true
   owners           = [data.azuread_client_config.current.object_id]
-
+#into this group adding owners to SPN we create and adding myself to AKS admin
   members = [
     data.azuread_client_config.current.object_id,
     azuread_service_principal.spn.object_id,
